e03ac8be89c777d57dc288b3987e524be5ca64f7
Vladislav <Vlad-izmestev@mail.ru>
2026-06-24 21:53:38 +0700

    111

--- src/router.js
+++ src/router.js
@@ -4,10 +4,10 @@ var routes = [
 	{ path: '/calc', name: 'Calc', meta: { title: 'Калькулятор' } },
 	{ path: '/table', name: 'Table', meta: { title: 'Таблица' } },
 	{ path: '/funcs', name: 'Funcs', meta: { title: 'Функции' } },
-	{ path: '/test', name: 'Test', meta: { title: 'Test' } },
-	{ path: '/test2', name: 'Test2', meta: { title: 'Test2' } },
-	{ path: '/game', name: 'Game2048', meta: { title: '2048' } },
-	{ path: '/psychosophy', name: 'Psychosophy', meta: { title: 'Psychosophy' } },
+	// { path: '/test', name: 'Test', meta: { title: 'Test' } },
+	// { path: '/test2', name: 'Test2', meta: { title: 'Test2' } },
+	// { path: '/game', name: 'Game2048', meta: { title: '2048' } },
+	// { path: '/psychosophy', name: 'Psychosophy', meta: { title: 'Psychosophy' } },
 
 	{ path: '/:catchAll(.*)', redirect: '/calc' },
 ]
